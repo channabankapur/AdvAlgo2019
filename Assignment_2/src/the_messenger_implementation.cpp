9ccb2b136202bc3d363144a476a50b8b99569651
Arvind Srinivasan <arvind.srini.8@gmail.com>
2019-10-10 17:51:28 +0530

    Added Question 2 Implementation File

---
+++ Assignment_2/src/the_messenger_implementation.cpp
@@ -0,0 +1,10 @@
+#include "../include/the_messenger.hpp"
+#include <vector>
+using namespace std;
+
+
+class the_messenger_implementation : public the_messenger
+{
+public:
+    int solve(int, int, int, string);
+};
